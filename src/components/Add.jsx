2407d0d1b029d6f8179a46af91e479573dcca354
김송하 <songha714@naver.com>
2024-09-11 23:28:57 +0900

    close #2

---
+++ src/components/Add.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+//import { Link } from "react-router-dom";
+
+const Add = () => {
+  const [board, setBoard] = useState({ title: "", content: "", id: 0 });
+  //const board = { id, title, content };
+  const [boardList, setBoardList] = useState([]);
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setBoard({ ...board, [name]: value });
+  };
+
+  // add 버튼을 눌렀을 때
+  const onClick = (e) => {
+    e.preventDefault();
+    if (board.title && board.content) {
+      setBoardList([...boardList, { ...board, id: boardList.length + 1 }]);
+      setBoard({ title: "", content: "", id: board.id + 1 });
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <h3>Title</h3>
+        <input
+          type="text"
+          value={board.title}
+          name="title"
+          onChange={handleInput}
+        />
+        <h3>content</h3>
+        <textarea
+          name="content"
+          value={board.content}
+          onChange={handleInput}
+        ></textarea>
+        <br />
+        <button onClick={onClick}>add</button>
+      </div>
+      <div>
+        <ul>
+          {boardList.map((board) => (
+            <li key={board.id}>{board.title}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Add;
